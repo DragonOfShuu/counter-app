1652eb83ba25dc997ef1256015955655c350944f
Dragon of Shuu <68718280+DragonOfShuu@users.noreply.github.com>
2025-10-19 03:42:52 -0600

    Title Bar is Now Platform Specific

--- src/components/title-bar/title-bar.ts
+++ src/components/title-bar/title-bar.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+import { isTauri } from '@tauri-apps/api/core';
 import { getCurrentWindow } from '@tauri-apps/api/window';
+import { platform } from '@tauri-apps/plugin-os';
 
 @Component({
   selector: 'app-title-bar',
@@ -8,6 +10,8 @@ import { getCurrentWindow } from '@tauri-apps/api/window';
   styleUrl: './title-bar.css'
 })
 export class TitleBar {
+  isAWindow = !["ios", "android"].includes(platform()) && isTauri();
+
   minimizeWindow() {
     getCurrentWindow().minimize();
   }
