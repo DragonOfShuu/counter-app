b7928f060385fd195d51d3c3d5c3ae7c38183287
isacubes <isacubes247@gmail.com>
2025-10-28 14:55:58 -0600

    placeholders for rust api

--- src-tauri/src/counter.rs
+++ src-tauri/src/counter.rs
@@ -15,47 +15,63 @@ pub struct Counter {
 }
 
 #[tauri::command]
-pub fn new_counter(counter: Couter) -> String {
-    
+pub fn new_counter(counter: Counter) -> Result<String, String> {
+    Ok("placeholder".to_string())
 }
 
 #[tauri::command]
 pub fn get_counter(id: String) -> Result<Counter, String> {
-    
+    Ok(
+        Counter {
+            name: "placeholder".to_string(),
+            count: 0,
+            color: "FFFFFF".to_string(),
+            defaultCount: 0,
+            defaultStep: 0,
+            dateCreated: 0,
+            dateModified: 0,
+        }
+    )
 }
 
 #[tauri::command]
-pub fn get_counter_ids() -> Vec<String> {
-
+pub fn get_counter_ids() -> Result<Vec<String>, String> {
+    Ok(vec![])
 }
 
 #[tauri::command]
 pub fn update_counter(id: String, counter: Counter) -> Result<(), String> {
 
+    Ok(())
 }
 
 #[tauri::command]
 pub fn delete_counter(id: String) -> Result<(), String> {
 
+    Ok(())
 }
 
 #[tauri::command]
 pub fn increment_counter(id: String, step: i64) -> Result<(), String> {
 
+    Ok(())
 }
 
 #[tauri::command]
 pub fn decrement_counter(id: String, step: i64) -> Result<(), String> {
 
+    Ok(())
 }
 
 #[tauri::command]
 pub fn reset_counter(id: String) -> Result<(), String> {
 
+    Ok(())
 }
 
 #[tauri::command]
 pub fn get_counter_value(id: String) -> Result<i64, String> {
 
+    Ok(0)
 }
 
