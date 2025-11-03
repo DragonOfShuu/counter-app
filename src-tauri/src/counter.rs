ddfbd2597ec5318e119cc315b12c6710bb0e3942
i <isacubes247@gmail.com>
2025-11-02 21:59:43 -0700

    added basic implementation comments for the main methods

--- src-tauri/src/counter.rs
+++ src-tauri/src/counter.rs
@@ -5,54 +5,73 @@ use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
 mod storage;
+use storage::store;
 
 mod types;
 use types::{CounterType, CounterTypeModifiable};
 
 #[tauri::command]
 pub fn new_counter(counter_data: CounterTypeModifiable) -> Result<Uuid, String> {
-    let counter = CounterType::from(counter_data);
-    
-
-    Err("Unable to create counter".to_string())
+    store(CounterType::from(counter_data))
 }
 
 #[tauri::command]
 pub fn get_counter(id: String) -> Result<CounterType, String> {
+    // implement in storage module
     Err("function is not implemented yet".to_string())
 }
 
 #[tauri::command]
 pub fn get_counter_ids() -> Result<Vec<String>, String> {
+    // implement in storage module
     Err("function is not implemented yet".to_string())
 }
 
 #[tauri::command]
-pub fn update_counter(id: String, counter: CounterTypeModifiable) -> Result<(), String> {
+pub fn update_counter(id: String, counter_data: CounterTypeModifiable) -> Result<(), String> {
+    // get counter from id
+    // update counter
+    // store counter
+    // return result
     Err("function is not implemented yet".to_string())
 }
 
 #[tauri::command]
 pub fn delete_counter(id: String) -> Result<bool, String> {
+    // implement in storage module
     Err("function is not implemented yet".to_string())
 }
 
 #[tauri::command]
 pub fn increment_counter(id: String, step: i64) -> Result<(), String> {
+    // get counter from id
+    // update counter
+    // store counter
+    // return result
     Err("function is not implemented yet".to_string())
 }
 
 #[tauri::command]
 pub fn decrement_counter(id: String, step: i64) -> Result<(), String> {
+    // get counter from id
+    // update counter
+    // store counter
+    // return result
     Err("function is not implemented yet".to_string())
 }
 
 #[tauri::command]
 pub fn reset_counter(id: String) -> Result<(), String> {
+    // get counter
+    // call reset method on counter
+    // store counter
+    // return result
     Err("function is not implemented yet".to_string())
 }
 
 #[tauri::command]
 pub fn get_counter_value(id: String) -> Result<i64, String> {
+    // get counter
+    // return count
     Err("function is not implemented yet".to_string())
 }
